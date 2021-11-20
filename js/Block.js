0b8ffe1f29ce8c7e4c24f55fb3d93e1c0207bb24
Kabiristhebest <87224954+Kabiristhebest@users.noreply.github.com>
2021-11-20 15:48:31 +0800

    Update Block.js

--- js/Block.js
+++ js/Block.js
@@ -9,7 +9,8 @@ class Block{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-         this.image= loadImage("block.png")
+        this.image= loadImage("block.png")
+        this.visibility=255;                              
         World.add(world, this.body);
       }
 
